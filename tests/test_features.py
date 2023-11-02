ddd80a9ae41ed1c72e3b945a6ee3c2111cc4224c
marloz <martynaslozys@gmail.com>
2023-11-02 10:22:59 +0200

    switch to csvs for test data

--- tests/test_features.py
+++ tests/test_features.py
@@ -4,7 +4,10 @@ from src.features import calculate_features
 
 
 def test_calculate_features(test_data_dir):
-    df = pd.read_parquet(test_data_dir + "/features/input.parquet")
-    expected = pd.read_parquet(test_data_dir + "/features/expected.parquet")
-    res = calculate_features(df, window_lengths=[10, 20])
+    """Expected:
+    - Dataframe sorted by symbol, date
+    - SMA features added for specified lengths"""
+    df = pd.read_csv(test_data_dir + "/features/input.csv")
+    expected = pd.read_csv(test_data_dir + "/features/expected.csv")
+    res = calculate_features(df, window_lengths=[10, 20]).reset_index(drop=True)
     pd.testing.assert_frame_equal(res, expected)
